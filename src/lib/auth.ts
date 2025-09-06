1c201907ff7af4abe23d855d6096923f245cf6fb
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-09-06 11:25:05 +0200

    Add middleware to redirect if you are not logged in

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,11 +4,18 @@ import { db } from "~/server/db";
 import * as schema from "~/server/db/schema";
 import { admin } from "better-auth/plugins";
 import { jwt } from "better-auth/plugins";
+import { env } from "~/env";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
     schema,
   }),
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+  },
   plugins: [admin(), jwt()],
 });
