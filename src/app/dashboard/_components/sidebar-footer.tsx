6e3b77f5733dbd63deb533420cfb8c2d39891c1b
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-09-07 11:03:33 +0200

    Implement the offline and online realtime indicator

--- src/app/dashboard/_components/sidebar-footer.tsx
+++ src/app/dashboard/_components/sidebar-footer.tsx
@@ -2,8 +2,12 @@
 
 import { authClient } from "~/lib/auth-client";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { socket } from "~/server/socket-client/socket";
+import { useEffect, useState } from "react";
+import { cn } from "~/lib/utils";
 
 export function SidebarFooterUser() {
+  const [isOnline, setIsOnline] = useState(false);
   const { data: session } = authClient.useSession();
   const initials = (session?.user?.name ?? "")
     .split(" ")
@@ -12,6 +16,23 @@ export function SidebarFooterUser() {
     .slice(0, 2)
     .join("")
     .toUpperCase();
+  useEffect(() => {
+    function onConnect() {
+      setIsOnline(true);
+    }
+    function onDisconnect() {
+      setIsOnline(false);
+    }
+    if (socket.connected) {
+      onConnect();
+    }
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  });
 
   return (
     <div className="flex items-center gap-3 p-2">
@@ -25,8 +46,15 @@ export function SidebarFooterUser() {
       <div className="min-w-0 flex-1">
         <p className="truncate text-sm font-medium">{session?.user?.name}</p>
         <div className="flex items-center gap-1">
-          <div className="h-2 w-2 rounded-full bg-green-500" />
-          <p className="text-muted-foreground text-xs">Online</p>
+          <div
+            className={cn(
+              "h-2 w-2 rounded-full",
+              isOnline ? "bg-green-500" : "bg-red-500",
+            )}
+          />
+          <p className="text-muted-foreground text-xs">
+            {isOnline ? "Online" : "Offline"}
+          </p>
         </div>
       </div>
     </div>
