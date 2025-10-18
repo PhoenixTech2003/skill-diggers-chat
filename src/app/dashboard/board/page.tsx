38c6902a0dbaf2b83a24fa11dce4ba2bdb181798
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-10-18 13:09:15 +0200

    Add redirect for people who are not in the feature

--- src/app/dashboard/board/page.tsx
+++ src/app/dashboard/board/page.tsx
@@ -1,8 +1,16 @@
 import { preloadQuery } from "convex/nextjs";
 import { api } from "../../../../convex/_generated/api";
 import { BoardPageClient } from "./_components/board-page-client";
+import { theBoardFlag } from "~/flags";
+import { redirect } from "next/navigation";
 
 export default async function BoardPageServer() {
+  const theBoard = await theBoardFlag();
+
+  if (!theBoard) {
+    redirect("/dashboard");
+  }
+
   const preloadedIssues = await preloadQuery(
     api.issues.getOpenAndApprovedIssues,
     {},
