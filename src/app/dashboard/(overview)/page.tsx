2e035c2236c0ca894068d2ea542dd8be90b7c8f0
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-10-21 17:26:17 +0200

    Add session check

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,8 +1,24 @@
 import { Suspense } from "react";
 import { DashboardContent } from "./_components/dashboard-content";
 import { LoadingSkeleton } from "./_components/loading-skeleton";
+import { fetchQuery } from "convex/nextjs";
+import { api } from "convex/_generated/api";
+import { getToken } from "~/lib/auth-server";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const token = await getToken();
+  const { sessionData, sessionDataError } = await fetchQuery(
+    api.users.getLoggedUserSession,
+    {},
+    { token },
+  );
+  if (sessionDataError) {
+    throw new Error(sessionDataError);
+  }
+  if (!sessionData?.session) {
+    redirect("/auth/login");
+  }
   return (
     <Suspense fallback={<LoadingSkeleton />}>
       <DashboardContent />
