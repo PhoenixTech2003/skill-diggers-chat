e06e705cc4fc41aec82a695258825b977883d112
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-10-26 21:49:02 +0200

    Use the internal auth token

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -5,7 +5,7 @@ import { env } from "~/env";
 export const authToken: string = env.VIDEO_SDK_API_KEY;
 
 // API call to create a meeting
-export const createMeeting = async ({ token }: { token: string }) => {
+export const createMeeting = async () => {
   try {
     const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
       method: "POST",
